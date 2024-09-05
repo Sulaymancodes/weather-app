b8978ac6375ca5f6df98c80892d4ec60a2100984
sulaiman-rabiu <sulaymancodes@gmail.com>
2024-09-05 21:27:30 +0100

    Data being fetched from visual crossing API

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('3:50AM')
+async function fetchWeather(params) {
+    const response =  await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=5TJ24EHEMB5B7V7VEBJH8H3WZ', {mode: 'cors'});
+    const weatherData = await response.json()
+    console.log(weatherData);
+}
+
+fetchWeather()
