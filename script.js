1cbba4c84f06b7a3cf191aea453d6c98a1e28f2c
sulaiman-rabiu <sulaymancodes@gmail.com>
2024-09-08 20:49:30 +0100

    Added Function to Process Data Received From API

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-async function fetchWeather(params) {
-    const response =  await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=5TJ24EHEMB5B7V7VEBJH8H3WZ', {mode: 'cors'});
+async function fetchWeather(locationName) {
+    const response =  await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locationName}?key=5TJ24EHEMB5B7V7VEBJH8H3WZ`, {mode: 'cors'});
     const weatherData = await response.json()
-    console.log(weatherData);
+    console.log(processWeatherInfo(weatherData))
 }
 
-fetchWeather()
+fetchWeather('lagos');
+
+function processWeatherInfo(data) {
+   return {
+      address: data.address, 
+      currentConditions: data.currentConditions,
+      days: data.days,
+      resolvedAddress: data.resolvedAddress
+   }
+}
